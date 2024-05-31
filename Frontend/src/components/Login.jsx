fc8dd5ce3f8a13872434f8c8a45c449c3f498c14
Akanksha Goswami <>
2024-05-31 10:53:50 +0530

    Frontend part end
    Frontend part are completed for my MERN Stack StoreBook Project.

---
+++ Frontend/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form"
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+  return (
+    <div>
+      <dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
+      {/* if there is a button in form, it will close the modal */}
+      <button to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+   
+    <h3 className="font-bold text-lg">Login</h3>
+    {/* <p className="py-4">Press ESC key or click on ✕ button to close</p> */}
+    <div className='mt-4 space-y-2'>
+        <span>Email</span><br/>
+        <input type="Email" placeholder='Enter Your Email' className='w-80 px-3 py-1 border rounded-md outline-none' 
+         {...register("email", { required: true })}/><br/>
+          {errors.email && <span className='text-sm text-red-500'>This field is required</span>}
+    </div>
+    <div className='mt-4 space-y-2'>
+        <span>Password</span><br/>
+        <input type="Text" placeholder='Enter Your Password' className='w-80 px-3 py-1 border rounded-md outline-none'
+         {...register("password", { required: true })} /><br/>
+          {errors.password && <span className='text-sm text-red-500'>This field is required</span>}
+    </div>
+    {/* Button */}
+    <div className='flex justify-around mt-4'>
+        <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>Login</button>
+        <p>Not registered? <Link to="/signup" className='underline text-blue-500 cursor-pointer'>Signup</Link> </p>
+    </div>
+    </form>
+  </div>
+</dialog>
+    </div>
+  )
+}
+
+export default Login
---
+++ Frontend/src/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+import Freebook from '../components/Freebook'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+     <Banner />
+     <Freebook/>
+     <Footer/>
+      
+    </>
+  )
+}
+
+export default Home
